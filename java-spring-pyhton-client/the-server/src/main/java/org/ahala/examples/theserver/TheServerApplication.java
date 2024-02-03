df6a5fdf8f2e181b1d5679e2c846ded6b77ee5f4
Adam Halassy <adam.halassy@gmail.com>
2024-02-03 08:43:37 +0100

    Added stub for REST client.

---
+++ java-spring-pyhton-client/the-server/src/main/java/org/ahala/examples/theserver/TheServerApplication.java
@@ -0,0 +1,13 @@
+package org.ahala.examples.theserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheServerApplication.class, args);
+	}
+
+}
---
+++ java-spring-pyhton-client/the-server/src/test/java/org/ahala/examples/theserver/TheServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahala.examples.theserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
